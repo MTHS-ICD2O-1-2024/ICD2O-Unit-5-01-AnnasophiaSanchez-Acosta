382d2c2ee35200ac410ff9c714f647270cf826c9
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-04-17 17:13:16 +0000

    prettier

--- js/script.js
+++ js/script.js
@@ -4,25 +4,31 @@
 // Created on: April 2025
 // This file contains the JS functions for index.html
 
-const randomNumber = Math.floor(Math.random()* 6) + 1
+const randomNumber = Math.floor(Math.random() * 6) + 1
 
 /**
  * This function checks if the user has selected the random number from above
  */
 //eslint-disable-next-line no-used-vars
-function checkNumber () {
-// input
-const userNumber = parseInt(document.getElementById(`user-number`).value)
-//process
-If (userNumber === randomNumber); {
-  //output
-  document.getElementById(`result`).innerHTML =
-  `<p>You have guessed the correct number!</p>`
-}
+function checkNumber() {
+  // input
+  const userNumber = parseInt(document.getElementById(`user-number`).value)
+  //process
+  If(userNumber === randomNumber)
+  {
+    //output
+    document.getElementById(
+      `result`
+    ).innerHTML = `<p>You have guessed the correct number!</p>`
+  }
 
-If (userNumber !== randomNumber); {
-  //output
-  document.getElementById(`result`).innerHTML =
-  `<p>You have guessed the worong number!` + `<br/> The correct number was` + randomNumber + `</p>`
-}
+  If(userNumber !== randomNumber)
+  {
+    //output
+    document.getElementById(`result`).innerHTML =
+      `<p>You have guessed the worong number!` +
+      `<br/> The correct number was` +
+      randomNumber +
+      `</p>`
+  }
 }
