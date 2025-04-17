d8aa2749bc5a58221dc2209707be8a1fc543014c
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-04-17 17:19:22 +0000

    fixed?

--- js/script.js
+++ js/script.js
@@ -9,26 +9,25 @@ const randomNumber = Math.floor(Math.random() * 6) + 1
 /**
  * This function checks if the user has selected the random number from above
  */
-//eslint-disable-next-line no-used-vars
 function checkNumber() {
   // input
-  const userNumber = parseInt(document.getElementById(`user-number`).value)
-  //process
+  const userNumber = parseInt(document.getElementById('user-number').value)
+  // process
   If(userNumber === randomNumber)
   {
-    //output
+    // output
     document.getElementById(
-      `result`
-    ).innerHTML = `<p>You have guessed the correct number!</p>`
+      'result'
+    ).innerHTML = '<p>You have guessed the correct number!</p>'
   }
 
   If(userNumber !== randomNumber)
   {
-    //output
-    document.getElementById(`result`).innerHTML =
-      `<p>You have guessed the worong number!` +
-      `<br/> The correct number was` +
+    // output
+    document.getElementById('result').innerHTML =
+      '<p>You have guessed the worong number!' +
+      '<br/> The correct number was' +
       randomNumber +
-      `</p>`
+      '</p>'
   }
 }
